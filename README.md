d160b5a1fdd69a011b3f0541825b2d50c47a34b5
Lucy Abramyan <abramyan@google.com>
2018-05-08 12:38:19 -0700

    Sceneform SDK for Android v1.0.0

---
+++ README.md
@@ -0,0 +1,29 @@
+Sceneform SDK for Android
+=====================
+Copyright (c) 2018 Google Inc.  All rights reserved.
+
+Sceneform is a 3D framework, with a physically based renderer, that's optimized for mobile, and that makes it easy for Java developers to build augmented reality apps.
+
+Please note, we do not accept pull requests.
+
+
+## Getting Started
+
+This repository contains Sceneform sample projects.
+
+To get started with the Sceneform SDK, follow the ARCore [Android Quickstart](//devsite.googleplex.com/ar/develop/java/quickstart) guide.
+
+
+## API Reference
+
+See the [Sceneform API Reference](//developers.google.com/ar/reference/java/com/google/ar/sceneform/package-summary).
+
+
+## Release Notes
+
+The SDK release notes are available on the [releases](//github.com/google-ar/sceneform-android-sdk/releases) page.
+
+
+## Terms & Conditions
+
+By downloading the Sceneform SDK for Android, you agree that the [Google APIs Terms of Service](//developers.google.com/terms/) governs your use thereof.
---
+++ solarsystem/app/src/main/java/com/google/ar/sceneform/samples/solarsystem/DemoUtils.java
@@ -0,0 +1,148 @@
+/*
+ * Copyright 2018 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.google.ar.sceneform.samples.solarsystem;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Handler;
+import android.os.Looper;
+import android.provider.Settings;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
+import android.view.Gravity;
+import android.widget.Toast;
+import com.google.ar.core.ArCoreApk;
+import com.google.ar.core.Config;
+import com.google.ar.core.Session;
+import com.google.ar.core.exceptions.UnavailableApkTooOldException;
+import com.google.ar.core.exceptions.UnavailableArcoreNotInstalledException;
+import com.google.ar.core.exceptions.UnavailableDeviceNotCompatibleException;
+import com.google.ar.core.exceptions.UnavailableException;
+import com.google.ar.core.exceptions.UnavailableSdkTooOldException;
+
+/** Static utility methods to simplify creating multiple demo activities. */
+public class DemoUtils {
+  private static final String TAG = "SceneformDemoUtils";
+  /**
+   * Creates and shows a Toast containing an error message. If there was an exception passed in it
+   * will be appended to the toast. The error will also be written to the Log
+   */
+  public static void displayError(
+      final Context context, final String errorMsg, @Nullable final Throwable problem) {
+    final String tag = context.getClass().getSimpleName();
+    final String toastText;
+    if (problem != null && problem.getMessage() != null) {
+      Log.e(tag, errorMsg, problem);
+      toastText = errorMsg + ": " + problem.getMessage();
+    } else if (problem != null) {
+      Log.e(tag, errorMsg, problem);
+      toastText = errorMsg;
+    } else {
+      Log.e(tag, errorMsg);
+      toastText = errorMsg;
+    }
+
+    new Handler(Looper.getMainLooper())
+        .post(
+            () -> {
+              Toast toast = Toast.makeText(context, toastText, Toast.LENGTH_LONG);
+              toast.setGravity(Gravity.CENTER, 0, 0);
+              toast.show();
+            });
+  }
+
+  /**
+   * Creates an ARCore session. This checks for the CAMERA permission, and if granted, checks the
+   * state of the ARCore installation. If there is a problem an exception is thrown. Care must be
+   * taken to update the installRequested flag as needed to avoid an infinite checking loop. It
+   * should be set to true if null is returned from this method, and called again when the
+   * application is resumed.
+   *
+   * @param activity - the activity currently active.
+   * @param installRequested - the indicator for ARCore that when checking the state of ARCore, if
+   *     an installation was already requested. This is true if this method previously returned
+   *     null. and the camera permission has been granted.
+   */
+  public static Session createArSession(Activity activity, boolean installRequested)
+      throws UnavailableException {
+    Session session = null;
+    // if we have the camera permission, create the session
+    if (hasCameraPermission(activity)) {
+      switch (ArCoreApk.getInstance().requestInstall(activity, !installRequested)) {
+        case INSTALL_REQUESTED:
+          return null;
+        case INSTALLED:
+          break;
+      }
+      session = new Session(activity);
+      // IMPORTANT!!!  ArSceneView needs to use the non-blocking update mode.
+      Config config = new Config(session);
+      config.setUpdateMode(Config.UpdateMode.LATEST_CAMERA_IMAGE);
+      session.configure(config);
+    }
+    return session;
+  }
+
+  /** Check to see we have the necessary permissions for this app, and ask for them if we don't. */
+  public static void requestCameraPermission(Activity activity, int requestCode) {
+    ActivityCompat.requestPermissions(
+        activity, new String[] {Manifest.permission.CAMERA}, requestCode);
+  }
+
+  /** Check to see we have the necessary permissions for this app. */
+  public static boolean hasCameraPermission(Activity activity) {
+    return ContextCompat.checkSelfPermission(activity, Manifest.permission.CAMERA)
+        == PackageManager.PERMISSION_GRANTED;
+  }
+  /** Check to see if we need to show the rationale for this permission. */
+  public static boolean shouldShowRequestPermissionRationale(Activity activity) {
+    return ActivityCompat.shouldShowRequestPermissionRationale(
+        activity, Manifest.permission.CAMERA);
+  }
+
+  /** Launch Application Setting to grant permission. */
+  public static void launchPermissionSettings(Activity activity) {
+    Intent intent = new Intent();
+    intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+    intent.setData(Uri.fromParts("package", activity.getPackageName(), null));
+    activity.startActivity(intent);
+  }
+
+  public static void handleSessionException(
+      Activity activity, UnavailableException sessionException) {
+
+    String message;
+    if (sessionException instanceof UnavailableArcoreNotInstalledException) {
+      message = "Please install ARCore";
+    } else if (sessionException instanceof UnavailableApkTooOldException) {
+      message = "Please update ARCore";
+    } else if (sessionException instanceof UnavailableSdkTooOldException) {
+      message = "Please update this app";
+    } else if (sessionException instanceof UnavailableDeviceNotCompatibleException) {
+      message = "This device does not support AR";
+    } else {
+      message = "Failed to create AR session";
+      Log.e(TAG, "Exception: " + sessionException);
+    }
+    Toast.makeText(activity, message, Toast.LENGTH_LONG).show();
+  }
+}
